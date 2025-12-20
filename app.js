4b0d26965a8ffd50c69f6183e0fd6e50d107e8d2
Malaika Shabbir <malaikashabbir1@gmail.com>
2025-12-21 00:25:09 +0100

    chore(structure): initial project structure for Community Help & Donation Platform

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req,res) => {
+  res.send("HELLO WORLD! I am here to help you and find the donors and for taking donations from the donors.");
+});
+
+app.listen(PORT, ()=>{
+  console.log(`Server is running on the port ${PORT}`);
+})
