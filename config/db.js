8022ab5f52ccb6491b48da2bfce46aa730a27504
Malaika Shabbir <malaikashabbir1@gmail.com>
2025-12-26 00:25:29 +0100

    chore: add MongoDB connection configuration in db.js

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/community_help_db', {
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    process.exit(1); // stop the server if DB connection fails
+  }
+};
+
+module.exports = connectDB;
