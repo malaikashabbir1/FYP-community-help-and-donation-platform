444028fa22a6f84334053bb104d02c9fdc746c66
Malaika Shabbir <malaikashabbir1@gmail.com>
2026-03-12 01:49:19 +0100

    feat: add pending routes and recent activity

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,11 +2,24 @@
 
 const express = require('express');
 const router = express.Router();
+const adminController = require('../controllers/adminController');
+
+// Import your middlewares
 const authenticateToken = require('../middlewares/authenticateToken');
 const authorizeRole = require('../middlewares/authorizeRole');
 
-router.get('/dashboard', authenticateToken, authorizeRole('admin'), (req, res) => {
-    res.render('admin/adminDashboard');
+// Admin Dashboard route
+router.get( '/dashboard',
+  authenticateToken,        // Check if user is logged in
+  authorizeRole('admin'),   // Check if role is admin
+  adminController.getAdminDashboard
+);
+// GET recent activity JSON
+router.get('/recent-activity-json', authenticateToken, authorizeRole('admin'), async (req, res) => {
+  const recentActivity = await ActivityLog.find()
+    .sort({ createdAt: -1 })
+    .limit(10)
+    .populate('userId', 'name role');
+  res.json(recentActivity);
 });
-
 module.exports = router;
