a9a0391c1bef34cb0e42307c2cb638080eb0c954
Malaika Shabbir <malaikashabbir1@gmail.com>
2026-05-04 13:35:13 +0100

    chore: improve flash message utility handling

--- utils/flashMessage.js
+++ utils/flashMessage.js
@@ -1,9 +1,3 @@
 exports.setMessage = (req, type, text) => {
   req.session.message = { type, text };
 };
-
-exports.getMessage = (req) => {
-  const message = req.session.message;
-  delete req.session.message; // clear after showing once
-  return message;
-};
