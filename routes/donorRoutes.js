571e8c1ac4e94872b374ade1d56591525f4896be
Malaika Shabbir <malaikashabbir1@gmail.com>
2026-05-09 15:24:32 +0100

    Implement role-based routing and dynamic navbar navigation system

--- routes/donorRoutes.js
+++ routes/donorRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const donorDonationController = require('../controllers/donorDonationController');
 const donorController = require('../controllers/donorController');
 const authenticateToken = require('../middlewares/authenticateToken');
 const authorizeRole = require('../middlewares/authorizeRole');
+const campaignController = require('../controllers/campaignController');
 
 
-// ===============================
+
 // DONOR DASHBOARD
-// ===============================
 router.get(
   '/dashboard',
   authenticateToken,
@@ -17,9 +18,7 @@ router.get(
 );
 
 
-// ===============================
 // DONATION FORM (FOR SPECIFIC CAMPAIGN)
-// ===============================
 router.get(
   '/donate/:campaignId',
   authenticateToken,
@@ -28,14 +27,17 @@ router.get(
 );
 
 
-// ===============================
 // SUBMIT DONATION
-// ===============================
 router.post(
   '/donate',
   authenticateToken,
   authorizeRole('donor'),
-  donorController.addDonation
+  donorDonationController.addDonation
 );
 
+// success stories 
+router.get('/campaigns/completed', campaignController.completedCampaigns);
+
+// MyDonations
+router.get('/my-donations', donorDonationController.getMyDonations);
 module.exports = router;
